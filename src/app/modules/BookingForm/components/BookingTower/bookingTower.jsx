fae8b33d33a85b069498fc7dc3068319cdb9fb90
pu6er <djakobstv@gmail.com>
2023-05-04 23:38:15 +0300

    Add onChange function to update the parent state

--- src/app/modules/BookingForm/components/BookingTower/bookingTower.jsx
+++ src/app/modules/BookingForm/components/BookingTower/bookingTower.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Form, Select } from "antd";
-const BookingTower = ({options}) => {
+const BookingTower = ({options, onChange, name}) => {
+  const handleChange = (value) => {
+    onChange(value,name)
+  }
   return (
     <Form.Item label="Башня">
         
           {/* <Select.Option value="">Demo</Select.Option> */}
-          <Select options={options} />
+          <Select options={options} onChange={handleChange} />
         
       </Form.Item>
    
