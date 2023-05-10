486da89f15daf8495b53c1ca1d296e29e4ffeb5d
pu6er <djakobstv@gmail.com>
2023-05-10 20:48:21 +0300

    Update component

--- src/app/modules/BookingForm/components/BookingComment/bookingComment.jsx
+++ src/app/modules/BookingForm/components/BookingComment/bookingComment.jsx
@@ -3,14 +3,14 @@ import { Form, Input } from "antd";
 
 
 
-const BookingComment = ({ onChange, name, }) => {
+const BookingComment = ({ onChange, name, value}) => {
   const handleChange = (e) => {
     onChange(e.target.value, name);
   };
   const {TextArea} = Input;
   return (
-    <Form.Item label="Комментарий">
-      <TextArea onChange={handleChange}  />
+    <Form.Item label="Комментарий" name={name}>
+      <TextArea onChange={handleChange}  value={value}/>
     </Form.Item>
   );
 };
