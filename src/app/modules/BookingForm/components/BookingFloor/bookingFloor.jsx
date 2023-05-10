50fce65df0b0a9ab024b5393a7ce8e7be247a907
pu6er <djakobstv@gmail.com>
2023-05-10 19:38:53 +0300

    Add validation and input handling

--- src/app/modules/BookingForm/components/BookingFloor/bookingFloor.jsx
+++ src/app/modules/BookingForm/components/BookingFloor/bookingFloor.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { Form, Select } from "antd";
 
-const BookingFloor = ({ onChange,name, options }) => {
+const BookingFloor = ({ onChange, name, options, value }) => {
   const handleChange = (value) => {
-    onChange(value,name)
-  }
+    onChange(value, name);
+  };
   return (
-    <Form.Item label="Этаж">
+    <Form.Item
+      label="Этаж"
+      name={name}
+      rules={[{ required: true, message: "Выберете этаж" }]}
+    >
       {/* <Select.Option value="">Demo</Select.Option> */}
-      <Select
-      
-      onChange={handleChange}
-        options={options}
-      />
+      <Select onChange={handleChange} options={options} value={value} />
     </Form.Item>
   );
 };
